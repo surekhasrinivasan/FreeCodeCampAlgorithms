51c36eb287567ce971a27cfb3f258df5c6567feb
surekha <surekhasrao@gmail.com>
2017-12-17 21:15:02 +0000

    Algorithm for caesarsCiphers.js

---
+++ caesarsCipher.js
@@ -0,0 +1,20 @@
+//Caesars Ciphers
+function rot13(str) { // LBH QVQ VG!
+  
+  var newString = [];
+
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) < 65 || str.charCodeAt(i) > 90) {
+      newString.push(str.charAt(i));
+      //console.log(newString.push(str.charAt(i)));
+    } else if (str.charCodeAt(i) > 77) {
+      newString.push(String.fromCharCode(str.charCodeAt(i) - 13));
+    } else {
+      newString.push(String.fromCharCode(str.charCodeAt(i) + 13));
+    }
+  }
+  return newString.join("");
+}
+// Change the inputs below to test
+console.log(rot13("SERR PBQR PNZC"));
+console.log(rot13("SERR CVMMN!"));
