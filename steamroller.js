115aad7a8d5a5b7e598dd5a09079246517c00f5f
surekha <surekhasrao@gmail.com>
2018-01-28 21:52:41 +0000

    Modified steamroller.js

--- steamroller.js
+++ steamroller.js
@@ -4,7 +4,9 @@ function steamrollArray(arr) {
   // I'm a steamroller, baby
   var newArray = [];
   flatten(arr);
-  
+
+// Create function that adds an element if it is not an array.
+  // If it is an array, then loops through it and uses recursion on that array.  
   function flatten(arr){
     arr.forEach(function(element){
       if(!Array.isArray(element)){
